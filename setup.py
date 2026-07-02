81fd2e70c3215e6a5bae27b333c98af7e7b49429
Rahul Hasan <careerrahulhasan93@gmail.com>
2026-07-02 19:05:05 +0530

    code updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rahul",
+    author_email="rahulhasanhelp@gmail.com",
+    packages=find_packages()
+)
